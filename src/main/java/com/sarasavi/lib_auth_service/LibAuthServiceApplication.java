1001b6235dd335e032fd850bfecee53b7b1fd956
chandisarandeni <chandisarandeni2003@gmail.com>
2025-07-08 20:34:21 +0530

    init: project with springboot ✅

---
+++ src/main/java/com/sarasavi/lib_auth_service/LibAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarasavi/lib_auth_service/LibAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
