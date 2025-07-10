264b445cb2a7146bd66e3b7c4ef8594597161e31
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-10 15:06:42 +0530

    feat: implemented the Librarian Entity feat: implemented the Librarian DTO ✅

--- src/main/java/com/sarasavi/lib_auth_service/dto/LibrarianDTO.java
+++ src/main/java/com/sarasavi/lib_auth_service/dto/LibrarianDTO.java
@@ -1,4 +1,18 @@
 package com.sarasavi.lib_auth_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibrarianDTO {
+    private int librarianId;
+    private String name;
+    private String nic;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String password;
 }
