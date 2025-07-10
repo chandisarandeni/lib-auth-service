61704fd98d9306d93e27465fbb75aa361973e262
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-10 14:31:33 +0530

    feat: Implement the Admin Controller ✅

--- src/main/java/com/sarasavi/lib_auth_service/controllers/AdminController.java
+++ src/main/java/com/sarasavi/lib_auth_service/controllers/AdminController.java
@@ -1,4 +1,24 @@
 package com.sarasavi.lib_auth_service.controllers;
 
+import com.sarasavi.lib_auth_service.dto.AdminDTO;
+import com.sarasavi.lib_auth_service.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/admins/auth")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    // admin authentication methods will be added here
+    @PostMapping(path = "/login")
+    public boolean login(@RequestBody AdminDTO adminDTO) {
+        // Call the service to authenticate the admin
+        return adminService.loginAdmin(adminDTO.getEmail(), adminDTO.getPassword());
+    }
 }
