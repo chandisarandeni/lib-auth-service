9eafbed802fb8d05506bdae2504d401ca1839c31
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-10 14:06:55 +0530

    Added the admin Repository

--- src/main/java/com/sarasavi/lib_auth_service/repository/AdminRepository.java
+++ src/main/java/com/sarasavi/lib_auth_service/repository/AdminRepository.java
@@ -1,6 +1,8 @@
 package com.sarasavi.lib_auth_service.repository;
 
+import com.sarasavi.lib_auth_service.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public class AdminRepository extends JpaRepository<Admin, Integer> {
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
 }
