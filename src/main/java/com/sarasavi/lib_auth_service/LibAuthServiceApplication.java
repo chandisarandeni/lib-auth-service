dc89241205e97c1add770ffb563795f49b118481
chandisarandeni <chandisarandeni2003@gmail.com>
2025-07-10 09:36:44 +0530

    feat: implement database connection ✅

--- src/main/java/com/sarasavi/lib_auth_service/LibAuthServiceApplication.java
+++ src/main/java/com/sarasavi/lib_auth_service/LibAuthServiceApplication.java
@@ -1,3 +1,10 @@
+// User Guide:
+// ***************************************************************************
+// **** Please make sure to follow the steps below to run the application ****
+// ***************************************************************************
+// Database creation script: CREATE DATABASE lib_auth_service_db;
+
+
 package com.sarasavi.lib_auth_service;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibAuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibAuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibAuthServiceApplication.class, args);
+    }
 
 }
