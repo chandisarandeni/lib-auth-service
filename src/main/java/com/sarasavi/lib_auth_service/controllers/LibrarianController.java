70e8d4b318143b3f8e098eee99386e3abc979cbf
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-10 15:20:41 +0530

    feat: implemented the Librarian controller  ✅

--- src/main/java/com/sarasavi/lib_auth_service/controllers/LibrarianController.java
+++ src/main/java/com/sarasavi/lib_auth_service/controllers/LibrarianController.java
@@ -1,4 +1,27 @@
 package com.sarasavi.lib_auth_service.controllers;
 
+
+import com.sarasavi.lib_auth_service.dto.LibrarianDTO;
+import com.sarasavi.lib_auth_service.entity.Librarian;
+import com.sarasavi.lib_auth_service.services.LibrarianService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/librarians/auth")
 public class LibrarianController {
+
+    @Autowired
+    private LibrarianService librarianService;
+
+    // librarian authentication methods will be added here
+    @PostMapping(path = "/login")
+    public boolean login(@RequestBody LibrarianDTO librarianDTO) {
+        // Call the service method to authenticate the librarian
+        return librarianService.loginLibrarian(librarianDTO.getEmail(), librarianDTO.getPassword());
+    }
+
 }
